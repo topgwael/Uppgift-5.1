bf69938808345f691c7d9ab6ab660dff46cd4cd2
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:11:09 +0100

    Add project files.

---
+++ Uppgift 5.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Array
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv hur många manm du vill ha");
+            int antal = int.Parse(Console.ReadLine());
+            string[] namn = new string[antal];
+
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine("skriv ett namn");
+                namn[i] = Console.ReadLine();
+            }
+            Console.WriteLine("här är namnet du skrev in");
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
